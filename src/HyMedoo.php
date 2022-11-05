a3a12c4f78f656fe8e8176a7fc6fcd49edf4e66f
Sgenmi <sgenmi@gmail.com>
2022-11-05 16:59:44 +0800

    重写，直接使用原始medoo

--- src/HyMedoo.php
+++ src/HyMedoo.php
@@ -1,41 +1,71 @@
 <?php
 
+declare(strict_types=1);
+
 /**
  * Created by IntelliJ IDEA.
  * Author: sgenmi
- * Date: 2020/5/22 下午2:51
+ * Date: 2022/11/5 4:53 PM
  * Email: 150560159@qq.com
- *
- * Medoo database framework
- * https://medoo.in
- * Version 1.7.10
- *
- * Copyright 2020, Angel Lai
- * Released under the MIT license
  */
 
 namespace Sgenmi\HyMedoo;
+
+use Hyperf\DB\DB;
 use Hyperf\Utils\ApplicationContext;
+use Medoo\Medoo;
 
-class HyMedoo {
+/**
+ * @method array select(array $columns, array $where)
+ * @method null select(array $columns, callable $callback)
+ * @method null select(array $columns, array $where, callable $callback)
+ * @method null select(array $join, array $columns, array $where, callable $callback)
+ * @method mixed get(array|string $columns, array $where)
+ * @method bool has(array $where)
+ * @method mixed rand(array|string $column, array $where)
+ * @method int count(array $where)
+ * @method int max(string $column)
+ * @method int min(string $column)
+ * @method int avg(string $column)
+ * @method int sum(string $column)
+ * @method int max(string $column, array $where)
+ * @method int min(string $column, array $where)
+ * @method int avg(string $column, array $where)
+ * @method int sum(string $column, array $where)
+ */
+abstract class HyMedoo
+{
+    protected string $table = '';
 
-    public function __construct()
+    public function __call(string $name, array $arguments)
     {
+        return self::call($name, $arguments);
     }
 
-    public function __call($name, $arguments)
+    public static function __callStatic(string $name, array $arguments)
     {
-        $container = ApplicationContext::getContainer();
-        $self = $container->get(Medoo::class);
-        return $self->{$name}(...$arguments);
+        return self::call($name, $arguments);
     }
 
-    public static function __callStatic($name,$arguments)
+    public function getTable(bool $isComplete = true): string
     {
-        $container = ApplicationContext::getContainer();
-        $self = $container->get(Medoo::class);
-        return $self->{$name}(...$arguments);
+        $prefix = config('db.default.prefix', '');
+        return $isComplete ? $prefix . $this->table : $this->table;
     }
 
-
+    private static function call(string $name, array $arguments)
+    {
+        $options = [
+            'database_type' => 'mysql',
+        ];
+        $table = (new static())->getTable();
+        return ApplicationContext::getContainer()->get(DB::class)->run(
+            function (\PDO $pdo) use ($options, $name, $arguments, $table) {
+                $options['pdo'] = $pdo;
+                $medoo = new Medoo($options);
+                array_unshift($arguments, $table);
+                return $medoo->{$name}(...$arguments);
+            }
+        );
+    }
 }
