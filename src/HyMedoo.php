5dd2979433116bb4e8030816488c1ed71b3bd143
Sgenmi <sgenmi@gmail.com>
2022-11-07 12:53:59 +0800

    add:debug,last()方法兼容

--- src/HyMedoo.php
+++ src/HyMedoo.php
@@ -11,29 +11,56 @@ declare(strict_types=1);
 
 namespace Sgenmi\HyMedoo;
 
+use Hyperf\Context\Context;
 use Hyperf\DB\DB;
 use Hyperf\Utils\ApplicationContext;
 use Medoo\Medoo;
+use Medoo\Raw;
 
 /**
- * @method array select(array $columns, array $where)
+ * @method insert(array $values)
+ * @method update(array $data,array $where=null)
+ * @method delete(array|Raw $where)
+ * @method replace(array $columns, array $where=null)
+ *
+ * @method array select( array $columns, array $where=null)
+ * @method array select( array $join,array $columns, array $where=null)
  * @method null select(array $columns, callable $callback)
  * @method null select(array $columns, array $where, callable $callback)
  * @method null select(array $join, array $columns, array $where, callable $callback)
- * @method mixed get(array|string $columns, array $where)
+ *
+ * @method mixed get(array|string $columns, array $where=null)
+ * @method mixed get(array $join, array|string $columns, array $where)
+ *
+ * @method void action(callable $actions )
+ *
  * @method bool has(array $where)
- * @method mixed rand(array|string $column, array $where)
- * @method int count(array $where)
- * @method int max(string $column)
- * @method int min(string $column)
- * @method int avg(string $column)
- * @method int sum(string $column)
- * @method int max(string $column, array $where)
- * @method int min(string $column, array $where)
- * @method int avg(string $column, array $where)
- * @method int sum(string $column, array $where)
+ * @method bool has(array $join, array $where)
+ *
+ * @method mixed rand(array|string $column, array $where=null)
+ * @method mixed rand(array $join,array|string $column, array $where)
+ *
+ * @method int count(string $column, array $where=null)
+ * @method int count(array $join, string $column, array $where)
+ *
+ * @method string max(string $column,array $where=null)
+ * @method string max(array $join,string $column,array $where)
+ *
+ * @method string min(string $column,array $where=null)
+ * @method string min(array $join,string $column, array $where)
+ *
+ * @method string avg(string $column,array $where=null)
+ * @method string avg(array $join,string $column,array $where)
+ *
+ * @method string sum(string $column,array $where=null)
+ * @method string sum(array $join,string $column,array $where)
+ *
+ * @method mixed debug();
+ * @method array log();
+ * @method array info();
+ *
  */
-abstract class HyMedoo
+abstract class Model
 {
     protected string $table = '';
 
@@ -53,19 +80,69 @@ abstract class HyMedoo
         return $isComplete ? $prefix . $this->table : $this->table;
     }
 
+    /**
+     * 获取最后一次执行的sql
+     * @return string
+     */
+    public function last(): string
+    {
+        return  strval(Context::get(static::class.":model:lastSql",''));
+    }
+
     private static function call(string $name, array $arguments)
     {
-        $options = [
-            'database_type' => 'mysql',
-        ];
-        $table = (new static())->getTable();
-        return ApplicationContext::getContainer()->get(DB::class)->run(
-            function (\PDO $pdo) use ($options, $name, $arguments, $table) {
+        $unionDebugKey = self::getUnionKey('model:debug');
+        $unionLastSqlKey = self::getUnionKey('model:lastSql');
+
+        $cls = ApplicationContext::getContainer()->get(static::class);
+        if($name=='debug'){
+            Context::set($unionDebugKey,true);
+            return $cls;
+        }
+        $table = $cls->getTable();
+        //debug() mode
+        $debug = Context::get($unionDebugKey);
+
+        $res =  ApplicationContext::getContainer()->get(DB::class)->run(
+            function (\PDO $pdo) use ($name, $arguments, $table,$debug,$unionLastSqlKey) {
+                $options = [
+                    'database_type' => 'mysql',
+                ];
+                $ret = null;
                 $options['pdo'] = $pdo;
                 $medoo = new Medoo($options);
                 array_unshift($arguments, $table);
-                return $medoo->{$name}(...$arguments);
+                //debug 返回medoo debug()方式
+                if($debug ){
+                    return $medoo->debug()->{$name}(...$arguments);
+                }
+
+                switch ($name){
+                    case 'insert':
+                        $stm = $medoo->{$name}(...$arguments);
+                        if($stm){
+                            $ret =  $medoo->id();
+                        }
+                        break;
+                    default:
+                        $ret = $medoo->{$name}(...$arguments);
+                }
+                Context::set($unionLastSqlKey,$medoo->last());
+                return $ret;
             }
         );
+
+        if(Context::has($unionDebugKey)){
+            Context::set($unionDebugKey,false);
+        }
+        return $res;
+    }
+
+    /**
+     * @param string $name
+     * @return string
+     */
+    private static function getUnionKey(string $name):string {
+        return static::class.':'.$name;
     }
 }
